fce4815c6ad57040789608f7a0ec408d4785740f
mfm6 <melissafm24@gmail.com>
2016-08-03 17:18:31 -0400

    tidied up a bit, separated test and training processing

--- data/NonlinearDynamicModel/PreprocessData.m
+++ data/NonlinearDynamicModel/PreprocessData.m
@@ -1,13 +1,12 @@
-function v_t =  PreprocessData ()
+function  PreprocessData ()
 
 data = load('Data_Set_Raw_500Flights_Altitude_5Sec_Noise_GE.mat');
 
 D_trval = 450;  % # of training/validation samples
-D_tst = 50;     % # of test samples
 fs = 10;        % Downsampling frequency
 len = 500;
 
-for i=(D_trval+1) : len
+for i = 0 : D_trval
     
     speedData = sum(data.DS{i}.X(13:end,:));
     speedData = speedData(:,1:fs:end);
@@ -24,7 +23,28 @@ for i=(D_trval+1) : len
     max_val = max(max(v_t));
     v_t = v_t/max_val;
     
-    dlmwrite('speedDataNormalizedTestData.csv', v_t, '-append');
+    dlmwrite('speedTrainingDataNormalized.csv', v_t, '-append');
+ 
+end
+
+for i = (D_trval+1) : len
+    
+    speedData = sum(data.DS{i}.X(13:end,:));
+    speedData = speedData(:,1:fs:end);
+    
+    speedData = speedData(:);
+    stepSize = 5;
+    indx = stepSize;
+   
+    for j = 1 : (size(speedData,1) - (stepSize-1))
+        v_t(j,:) = speedData(j: indx);
+        indx = indx + 1;
+    end
+    % Normalization
+    max_val = max(max(v_t));
+    v_t = v_t/max_val;
+    
+    dlmwrite('speedTestDataNormalized.csv', v_t, '-append');
  
 end
 
